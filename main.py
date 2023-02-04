188053b248317bfe180d638a2ba5fa013815bd88
Bar Snir <bars@Admins-MacBook-Pro.local>
2023-02-05 00:26:44 +0200

    orgniaze main.py

--- main.py
+++ main.py
@@ -4,18 +4,26 @@ from pyflink.table import TableEnvironment, EnvironmentSettings
 
 def get_jars_path():
     path = os.getcwd()
-    return f'file://{path}/jars/kafka_jars'
+    return f'file://{path}/jars/kafka_jars/'
 
-def log_processing():
-    env_settings = EnvironmentSettings.in_streaming_mode()
-    t_env = TableEnvironment.create(env_settings)
-    jars_path = get_jars_path()
-    t_env.get_config().set(
-      'pipeline.jars',
-      f'{jars_path}/flink-sql-connector-kafka-1.16.0.jar;{jars_path}/flink-sql-avro-1.16.0.jar;{jars_path}/flink-sql-avro-confluent-registry-1.16.0.jar'
-    )
-    table_name = os.getenv('TOPIC')
-    source_ddl = f"""
+def get_env(key:str, default:str) -> str: 
+  return os.getenv(key, default)
+
+def get_jars_full_path() -> str:
+  jars_path = get_jars_path()
+  jars = [
+    'flink-sql-connector-kafka-1.16.0.jar;',
+    'flink-sql-avro-1.16.0.jar;',
+    'flink-sql-avro-confluent-registry-1.16.0.jar'
+  ]
+  full_str = ''
+  for jar in jars:
+    full_str = f'{full_str}{jars_path}{jar}'
+  return full_str
+
+def get_table_creation_string():
+    table_name = get_env('TOPIC', 'my_topic')
+    return f"""
             CREATE TABLE {table_name} (
                 token VARCHAR
             ) WITH (
@@ -31,29 +39,40 @@ def log_processing():
                 'properties.sasl.mechanism' = 'PLAIN',
                 'properties.security.protocol' = 'SASL_SSL',
                 'scan.startup.mode' = 'latest-offset',
-                'topic' = '{os.getenv('TOPIC')}'
+                'topic' = '{table_name}'
             )
             """
+  
 
-    # sink_ddl = """
-    #         CREATE TABLE sink_table(
-    #             a VARCHAR
-    #         ) WITH (
-    #           'connector' = 'kafka',
-    #           'topic' = 'sink_topic',
-    #           'properties.bootstrap.servers' = 'kafka:9092',
-    #           'format' = 'json'
-    #         )
-    #         """
+def log_processing():
+    env_settings = EnvironmentSettings.new_instance() \
+      .with_built_in_catalog_name(get_env('CATALOG_NAME', 'my_catalog')) \
+      .with_built_in_database_name(get_env('DATABASE_NAME', 'my_database')) \
+      .in_streaming_mode().build()
+  
+    t_env = TableEnvironment.create(env_settings)
+    
+    t_env.get_config().set('pipeline.jars',get_jars_full_path()) \
+      .set("parallelism.default", get_env('PARALLELISM', '1'))
 
+    source_ddl = get_table_creation_string()
     t_env.execute_sql(source_ddl)
-    table = t_env.from_path(table_name)
+    table = t_env.from_path(get_env('TOPIC', 'my_topic'))
     table.execute().print()
 
-    # t_env.execute_sql(sink_ddl)
-
-    # t_env.sql_query("SELECT a FROM source_table") \
-    #     .execute_insert("sink_table").wait()
+  # sink_ddl = """
+  #         CREATE TABLE sink_table(
+  #             a VARCHAR
+  #         ) WITH (
+  #           'connector' = 'kafka',
+  #           'topic' = 'sink_topic',
+  #           'properties.bootstrap.servers' = 'kafka:9092',
+  #           'format' = 'json'
+  #         )
+  #         """
+  # t_env.execute_sql(sink_ddl)
+  # t_env.sql_query("SELECT a FROM source_table") \
+  #     .execute_insert("sink_table").wait()
 
 
 if __name__ == '__main__':
